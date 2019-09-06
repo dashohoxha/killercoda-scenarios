b5dd9724f683bcdcdf420bf12847f432e47ccea2
Dashamir Hoxha <dashohoxha@gmail.com>
2019-09-06 06:14:21 +0200

    Wait for DVC to be installed

--- data-management/install-dvc.sh
+++ data-management/install-dvc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-wget https://dvc.org/deb/dvc.list -O /etc/apt/sources.list.d/dvc.list
-apt update
-apt install --yes dvc
+#wget https://dvc.org/deb/dvc.list -O /etc/apt/sources.list.d/dvc.list
+#apt update
+#apt install --yes dvc
 
-# pip install dvc
+pip install dvc
+
+echo "done" >> /var/.dvc-installed
