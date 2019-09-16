11d6de34b304a7af93fb0cd72101738d75f0c4e2
Dashamir Hoxha <dashohoxha@gmail.com>
2019-09-16 22:42:41 +0200

    Test

--- data-management/install-dvc.sh
+++ data-management/install-dvc.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-#wget https://dvc.org/deb/dvc.list -O /etc/apt/sources.list.d/dvc.list
-#apt update
-#apt install --yes dvc
-
 pip install dvc
-
-dvc version
-
-#echo "done" >> /var/.dvc-installed
+echo "done" >> /var/.dvc-installed
